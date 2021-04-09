0d1e1981215cb378b85aaa3a45a3347bbd90dca3
Bakhtiyor Esanov <bahtiyoresanov@gmail.com>
2021-04-09 03:23:22 +0300

    Completed module

--- GradeBook/GradeBooks/StandardGradeBook.cs
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -4,9 +4,9 @@ using System.Text;
 
 namespace GradeBook.GradeBooks
 {
-    public class StandartGradeBook : BaseGradeBook
+    public class StandardGradeBook : BaseGradeBook
     {
-        public StandartGradeBook(string name):base(name)
+        public StandardGradeBook(string name):base(name)
         {
             Type = Enums.GradeBookType.Standard;
         }
